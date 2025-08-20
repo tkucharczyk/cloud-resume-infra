8aba8b8aa761dd9d56a488978ccf16e06b9ba197
tkucharczyk <tomekqharczyk@proton.me>
2025-08-20 20:13:13 +0200

    Add CloudFront CNAME record to DNS configuration

--- dns.tf
+++ dns.tf
@@ -13,3 +13,11 @@ resource "ovh_domain_zone_record" "cert_validation" {
   ttl       = 600
   target    = each.value.value
 }
+
+resource "ovh_domain_zone_record" "cloudfront_cname" {
+  zone      = "tkuresume.pl"
+  subdomain = "cv"
+  fieldtype = "CNAME"
+  ttl       = 3600
+  target    = "${aws_cloudfront_distribution.my_cf.domain_name}."
+}
